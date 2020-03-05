f6bce90e72d3f3d52a25787bdebbac356ed06bd8
bouveyronn <bouveyronn@gmail.com>
2020-03-05 13:58:58 +0100

    Ajout TU Employe et test intégration repository

---
+++ src/main/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,66 @@
+package com.ipiecoles.java.java350.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDate;
+
+public class EmployeTest {
+
+    @Test
+    public void getNombreAnneeAncienneteNow(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now());
+
+        //When
+        Integer anneeAnciennete = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertEquals(0, anneeAnciennete.intValue());
+    }
+
+    @Test
+    public void getNombreAnneeAncienneteNminus2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().minusYears(2L));
+
+        //When
+        Integer anneeAnciennete = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertEquals(2, anneeAnciennete.intValue());
+    }
+
+    @Test
+    public void getNombreAnneeAncienneteNull(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(null);
+
+        //When
+        Integer anneeAnciennete = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertEquals(0, anneeAnciennete.intValue());
+    }
+
+    @Test
+    public void getNombreAnneeAncienneteNplus2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().plusYears(2L));
+
+        //When
+        Integer anneeAnciennete = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertEquals(0, anneeAnciennete.intValue());
+    }
+
+
+
+}
